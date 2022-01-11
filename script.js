10bf415124169e6a28b348c66a8048afe31853e3
Sans3108 <dragnerobertleonard13@gmail.com>
2022-01-11 02:40:31 +0200

    aaaaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -1,45 +1,117 @@
 function roundToTwo(n) {
   return +(Math.round(n + "e+2") + "e-2");
 }
-
+const maxLen = 10;
 let gradeArr = [];
+let thesis = 0;
 
 function addGrade(g) {
+  if (gradeArr.length >= maxLen) {
+    return alert(`De unde atatea note prietene? Hai las-o incolo de activitate...`)
+  }
   gradeArr.push(g);
   update();
 }
 
 function reset() {
   gradeArr = [];
+  thesis = 0;
   update();
+  effect(false);
+  thesisToggle(true);
 }
 
 function update() {
   let calc = document.getElementById('calc');
   let result = document.getElementById('result');
 
-  if (!gradeArr[0]) {
-    calc.innerText = 'Formula: suma notelor / numarul de note (rotunjit la 2 decimale)\nNotele tale vor aparea aici, aplicate in formula';
-    result.innerText = 'Si media va fi afisata aici';
+  if (!gradeArr[0] && thesis === 0) {
+    calc.innerText = 'Notele tale vor aparea aici si vor fi puse in formula';
+    result.innerText = 'Iar media va fi afisata aici';
     return;
   }
 
-  let formulaText = `Formula: suma notelor / numarul de note (rotunjit la 2 decimale)\nMedia ta este:`;
-  let formula = formulaText + ` (${gradeArr.join(' + ')}) / ${gradeArr.length} =`;
+  let preText = `Media ta este:`;
 
-  if (gradeArr.length === 1) {
-    calc.innerText = formulaText;
-    result.innerText = `${gradeArr[0]}`;
-    return;
+  calc.innerText = `${preText} ${makeFormulaString(gradeArr, thesis)}`;
+  result.innerText = calculateMean(gradeArr, thesis);
+  effect();
+}
+
+function thesisToggle(s = false) {
+  let toggleBtn = document.getElementById('toggleBtn');
+  let grid = document.getElementById('gradeGrid');
+
+  if (s) {
+    toggleBtn.innerText = 'Nu'
+    toggleBtn.className = 'thesis-btn-red';
+    grid.className = 'grade-grid hidden'
+    thesis = 0;
+  } else {
+    toggleBtn.innerText = toggleBtn.innerText === 'Da' ? 'Nu' : 'Da';
+    toggleBtn.className = toggleBtn.innerText === 'Da' ? 'thesis-btn-green' : 'thesis-btn-red';
+    grid.className = grid.className === 'grade-grid hidden' ? 'grade-grid' : 'grade-grid hidden'
+    if (toggleBtn.innerText === 'Nu') thesis = 0;
   }
+  update();
+}
 
-  calc.innerText = formula;
+function setThesis(g) {
+  thesis = g;
+  update();
+}
+
+function calculateMean(grd, t) {
+  if (!grd[0]) {
+    if (t !== 0) return t;
+  }
 
   let val = 0;
+  grd.forEach(i => val += i);
 
-  gradeArr.forEach(grade => {
-    val += grade;
-  });
+  let mean = roundToTwo(val / grd.length);
 
-  result.innerText = roundToTwo(val / gradeArr.length);
+  if (t !== 0) mean = Math.round(roundToTwo(((mean * 3) + t) / 4));
+  return mean;
+}
+
+function makeFormulaString(grd, t) {
+  if (t === 0) {
+    if (grd[1]) return `(${grd.join(' + ')}) / ${grd.length} =`;
+    return '';
+  }
+
+  if (!grd[0] && t !== 0) return '';
+
+  if (grd[1]) return `((${grd.join(' + ')}) / ${grd.length} * 3 + ${t}) / 4 =`;
+  return `(${grd[0]} / ${grd.length} * 3 + ${t}) / 4 =`;
+}
+
+function effect(s = true) {
+  let result = document.getElementById('result');
+  let mean = calculateMean(gradeArr, thesis);
+
+  if (!s) {
+    result.className = '';
+    return;
+  }
+
+  if (mean < 5) {
+    result.className = 'effect-red';
+    return;
+  }
+  if (mean >= 5 && mean < 6) {
+    result.className = 'effect-yellow';
+    return;
+  }
+  if (mean >= 6 && mean < 9) {
+    result.className = 'effect-blue';
+    return;
+  }
+  if (mean >= 9 && mean < 10) {
+    result.className = 'effect-green';
+    return;
+  }
+  result.className = 'effect-rainbow';
+  return;
 }
